80849ba312dca041423e616c2581ee4bac65f79e
Samii Shabuse <sshabuse@gmail.com>
2026-05-16 14:00:56 -0400

    chore: init keyword file

---
+++ src/baseline_keyword.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def build_keyword_index(csv):
+    pass
+
+def search_keyword():
+    pass
+
+if __name__ == "__main__":
+    pass
